4156bfd13d33f51c958d0adb04a4a8c57775805a
GoncaloV <41482@alunos.isel.ip.pt>
2017-05-13 20:31:54 +0100

    Exercise 5 done

--- TP1/src/main.java
+++ TP1/src/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        Ex3.symbolOccurences(args[0]);
-        Ex3.firstSymbolPerLineOccurences(args[0]);
+        //Ex3.symbolOccurences(args[0]);
+        //Ex3.firstSymbolPerLineOccurences(args[0]);
         Ex3.symbolsAfterLetter(args[0], 'c');
         Ex3.symbolsAfterLetter(args[0], 'h');
         Ex3.symbolsAfterLetter(args[0], 'q');
